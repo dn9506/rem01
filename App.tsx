8dad51c26a9b389b8090dcfb6eaa12bf292735e3
Dmytro Nasekin <dnasekin@gmail.com>
2022-12-03 03:07:11 +0200

    Added Navbar

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Navbar} from './src/Navbar'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello React Native</Text>
-      <StatusBar style="auto" />
+      <Navbar title={'Todo App!'}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   
   },
 });
